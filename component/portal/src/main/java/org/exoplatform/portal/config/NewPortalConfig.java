192f69f0799f55d9081d2f688eb6afb5a5b746e1
Boubaker Khanfir <boubaker.khanfir@gmail.com>
2025-02-01 11:59:22 +0100

    feat: Allow to Restore Default Sites Layout and Pages - MEED-8268 - Meeds-io/MIPs#175

--- component/portal/src/main/java/org/exoplatform/portal/config/NewPortalConfig.java
+++ component/portal/src/main/java/org/exoplatform/portal/config/NewPortalConfig.java
@@ -23,170 +23,174 @@ import java.util.HashSet;
 
 import lombok.ToString;
 
-
-/**
- * Author : Nhu Dinh Thuan nhudinhthuan@yahoo.com May 23, 2006
- */
 @ToString
-public class NewPortalConfig {
+public class NewPortalConfig implements Cloneable {
 
-    private HashSet<String> predefinedOwner = new HashSet<String>(5);
+  private HashSet<String> predefinedOwner     = new HashSet<>();
 
-    private String ownerType;
+  private String          ownerType;
 
-    private String templateName;
+  private String          templateName;
 
-    private String location;
+  private String          location;
 
-    private String label;
+  private String          label;
 
-    private String description;
+  private String          description;
 
-    private String importMode;
+  private String          importMode;
 
-    /*
-     * Object type is used for override attribute in order to support both string and boolean types of the corresponding configuration field for backward compatibility reasons. 
-     */
-    private Object override;
+  /*
+   * Object type is used for override attribute in order to support both string
+   * and boolean types of the corresponding configuration field for backward
+   * compatibility reasons.
+   */
+  private Object          override;
 
-    private boolean useMetaPortalLayout = false;
+  private boolean         useMetaPortalLayout = false;
 
-    /**
-     * @deprecated use the location instead
-     */
-    @Deprecated
-    private String templateLocation;
+  /**
+   * @deprecated use the location instead
+   */
+  @Deprecated
+  private String          templateLocation;
 
-    public NewPortalConfig() {
+  public NewPortalConfig() {
+  }
+
+  public NewPortalConfig(NewPortalConfig cfg) {
+    this.ownerType = cfg.ownerType;
+    this.templateLocation = cfg.templateLocation;
+    this.location = cfg.location;
+    this.label = cfg.label;
+    this.description = cfg.description;
+    this.templateName = cfg.templateName;
+    this.predefinedOwner = new HashSet<>(cfg.predefinedOwner);
+    this.importMode = cfg.importMode;
+    this.override = cfg.override;
+    this.useMetaPortalLayout = cfg.useMetaPortalLayout;
+  }
+
+  public NewPortalConfig(String path) {
+    this.location = path;
+  }
+
+  public HashSet<String> getPredefinedOwner() {
+    return predefinedOwner;
+  }
+
+  public void setPredefinedOwner(HashSet<String> s) {
+    this.predefinedOwner = s;
+  }
+
+  /**
+   * @return the location
+   */
+  public String getLocation() {
+    return location;
+  }
+
+  /**
+   * @param location the location to set
+   */
+  public void setLocation(String location) {
+    this.location = location;
+  }
+
+  public String getTemplateLocation() {
+    if (location != null)
+      return location;
+    else
+      return templateLocation;
+  }
+
+  public void setTemplateLocation(String s) {
+    this.location = s;
+    this.templateLocation = s;
+  }
+
+  public String getTemplateName() {
+    return templateName;
+  }
+
+  public void setTemplateName(String s) {
+    this.templateName = s;
+  }
+
+  public boolean isPredefinedOwner(String user) {
+    return predefinedOwner.contains(user);
+  }
+
+  public String getOwnerType() {
+    return ownerType;
+  }
+
+  public void setOwnerType(String ownerType) {
+    this.ownerType = ownerType;
+  }
+
+  public String getLabel() {
+    return label;
+  }
+
+  public void setLabel(String label) {
+    this.label = label;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public String getImportMode() {
+    return importMode;
+  }
+
+  public void setImportMode(String importMode) {
+    this.importMode = importMode;
+  }
+
+  public Boolean getOverrideMode() {
+    if (override != null) {
+      return override instanceof String ? "true".equals((String) override) : (Boolean) override;
     }
+    return null;
+  }
 
-    public NewPortalConfig(NewPortalConfig cfg) {
-        this.ownerType = cfg.ownerType;
-        this.templateLocation = cfg.templateLocation;
-        this.location = cfg.location;
-        this.label = cfg.label;
-        this.description = cfg.description;
-        this.templateName = cfg.templateName;
-        this.predefinedOwner = new HashSet<String>(cfg.predefinedOwner);
-        this.importMode = cfg.importMode;
-        this.override = cfg.override;
-        this.useMetaPortalLayout = cfg.useMetaPortalLayout;
-    }
+  public void setOverrideMode(boolean overrideMode) {
+    this.override = overrideMode;
+  }
 
-    public NewPortalConfig(String path) {
-        this.location = path;
-    }
+  public void setUseMetaPortalLayout(boolean useMetaPortalLayout) {
+    this.useMetaPortalLayout = useMetaPortalLayout;
+  }
 
-    public HashSet<String> getPredefinedOwner() {
-        return predefinedOwner;
-    }
+  public boolean isUseMetaPortalLayout() {
+    return useMetaPortalLayout;
+  }
 
-    public void setPredefinedOwner(HashSet<String> s) {
-        this.predefinedOwner = s;
-    }
+  /**
+   * @deprecated the 'default' portal notion doesn't exist anymore
+   */
+  @Deprecated(forRemoval = true, since = "1.5.0")
+  public void setUseDefaultPortalLayout(boolean useMetaPortalLayout) {
+    this.useMetaPortalLayout = useMetaPortalLayout;
+  }
 
-    /**
-     * @return the location
-     */
-    public String getLocation() {
-        return location;
-    }
+  /**
+   * @deprecated the 'default' portal notion doesn't exist anymore
+   */
+  @Deprecated(forRemoval = true, since = "1.5.0")
+  public boolean isUseDefaultPortalLayout() {
+    return useMetaPortalLayout;
+  }
 
-    /**
-     * @param location the location to set
-     */
-    public void setLocation(String location) {
-        this.location = location;
-    }
+  @Override
+  protected NewPortalConfig clone() { // NOSONAR
+    return new NewPortalConfig(this);
+  }
 
-    public String getTemplateLocation() {
-        if (location != null)
-            return location;
-        else
-            return templateLocation;
-    }
-
-    public void setTemplateLocation(String s) {
-        this.location = s;
-        this.templateLocation = s;
-    }
-
-    public String getTemplateName() {
-        return templateName;
-    }
-
-    public void setTemplateName(String s) {
-        this.templateName = s;
-    }
-
-    public boolean isPredefinedOwner(String user) {
-        return predefinedOwner.contains(user);
-    }
-
-    public String getOwnerType() {
-        return ownerType;
-    }
-
-    public void setOwnerType(String ownerType) {
-        this.ownerType = ownerType;
-    }
-
-    public String getLabel() {
-        return label;
-    }
-
-    public void setLabel(String label) {
-        this.label = label;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getImportMode() {
-        return importMode;
-    }
-
-    public void setImportMode(String importMode) {
-        this.importMode = importMode;
-    }
-
-    public Boolean getOverrideMode() {
-      if (override != null) {
-        return override instanceof String ? "true".equals((String) override) : (Boolean) override;
-      }
-      return null;
-    }
-
-    public void setOverrideMode(boolean overrideMode) {
-      this.override = overrideMode;
-    }
-
-    public void setUseMetaPortalLayout(boolean useMetaPortalLayout) {
-      this.useMetaPortalLayout = useMetaPortalLayout;
-    }
-
-    public boolean isUseMetaPortalLayout() {
-      return useMetaPortalLayout;
-    }
-
-    /**
-     * @deprecated the 'default' portal notion doesn't exist anymore
-     */
-    @Deprecated(forRemoval = true, since = "1.5.0")
-    public void setUseDefaultPortalLayout(boolean useMetaPortalLayout) {
-      this.useMetaPortalLayout = useMetaPortalLayout;
-    }
-
-    /**
-     * @deprecated the 'default' portal notion doesn't exist anymore
-     */
-    @Deprecated(forRemoval = true, since = "1.5.0")
-    public boolean isUseDefaultPortalLayout() {
-      return useMetaPortalLayout;
-    }
 }
