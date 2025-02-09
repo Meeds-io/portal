7561047fd5b3bd2c86852d73aa83894b20631252
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2025-02-09 06:30:15 +0100

    feat: Set back custom CSS feature from branding page - MEED-8427 - Meeds-io/meeds#2939 (#1065)
    This change will set back a removed feature due to existing styling
    customization in already live productions. This behavior will be
    disabled by default for new platforms but can be enabled through a
    feature flag.

--- component/api/src/main/java/org/exoplatform/portal/branding/model/Branding.java
+++ component/api/src/main/java/org/exoplatform/portal/branding/model/Branding.java
@@ -109,6 +109,10 @@ public class Branding implements Serializable {
 
   @Getter
   @Setter
+  private String              customCss;
+
+  @Getter
+  @Setter
   private Map<String, String> themeStyle       = new HashMap<>();
 
   @Getter
