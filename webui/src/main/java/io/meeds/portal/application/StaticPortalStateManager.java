1e4ef0792d6c57d7db7e91ee192d57986fa86281
Boubaker Khanfir <boubaker.khanfir@gmail.com>
2025-02-19 16:55:35 +0100

    feat: Enable WebUI Stateless Mode by default - Meeds-io/MIPs#181

--- webui/src/main/java/io/meeds/portal/application/StaticPortalStateManager.java
+++ webui/src/main/java/io/meeds/portal/application/StaticPortalStateManager.java
@@ -19,59 +19,32 @@
  */
 package io.meeds.portal.application;
 
-import org.exoplatform.commons.api.settings.ExoFeatureService;
-import org.exoplatform.container.ExoContainerContext;
-import org.exoplatform.portal.application.PortalApplication;
-import org.exoplatform.portal.application.PortalRequestContext;
 import org.exoplatform.portal.application.PortalStateManager;
 import org.exoplatform.webui.application.WebuiApplication;
 import org.exoplatform.webui.application.WebuiRequestContext;
-import org.exoplatform.webui.application.portlet.PortletRequestContext;
 import org.exoplatform.webui.core.UIApplication;
 
-import io.meeds.portal.application.model.StaticApplicationState;
-
-import jakarta.servlet.ServletContext;
 import lombok.SneakyThrows;
 
 public class StaticPortalStateManager extends PortalStateManager {
 
-  private static final String PORTAL_STATIC_WEB_UI_FEATURE = "portal.staticWebUIComponents";
-
-  private static final String APPLICATION_ATTRIBUTE_PREFIX = "portal.";
-
-  private ExoFeatureService   featureService;
+  private UIApplication uiapp;
 
   @Override
   @SneakyThrows
   public UIApplication restoreUIRootComponent(WebuiRequestContext context) {
-    if (getFeatureService().isActiveFeature(PORTAL_STATIC_WEB_UI_FEATURE)) {
-      context.setStateManager(this);
+    context.setStateManager(this);
+    if (uiapp == null) {
       WebuiApplication app = (WebuiApplication) context.getApplication();
-
-      ServletContext servletContext = getServletContext();
-      String key = getStaticKey(context);
-
-      StaticApplicationState appState = null;
-      if (servletContext != null) {
-        appState = (StaticApplicationState) servletContext.getAttribute(APPLICATION_ATTRIBUTE_PREFIX + key);
-      }
-      return appState == null ? createStaticUIApplication(context, app) : appState.getApplication();
-    } else {
-      return super.restoreUIRootComponent(context);
+      uiapp = createStaticUIApplication(context, app);
     }
+    return uiapp;
   }
 
   @Override
   public void storeUIRootComponent(WebuiRequestContext context) {
-    if (getFeatureService().isActiveFeature(PORTAL_STATIC_WEB_UI_FEATURE)) {
-      UIApplication uiapp = context.getUIApplication();
-      if (uiapp != null) {
-        getServletContext().setAttribute(APPLICATION_ATTRIBUTE_PREFIX + getStaticKey(context),
-                                         new StaticApplicationState(uiapp));
-      }
-    } else {
-      super.storeUIRootComponent(context);
+    if (uiapp == null) {
+      uiapp = context.getUIApplication();
     }
   }
 
@@ -86,25 +59,6 @@ public class StaticPortalStateManager extends PortalStateManager {
                                                                                true,
                                                                                Thread.currentThread().getContextClassLoader());
     return app.createUIComponent(type, null, null, context);
-  }
-
-  private String getStaticKey(WebuiRequestContext webuiRC) {
-    if (webuiRC instanceof PortletRequestContext portletRC) {
-      return portletRC.getApplication().getApplicationId() + "/" + portletRC.getWindowId();
-    } else {
-      return PortalApplication.PORTAL_APPLICATION_ID;
-    }
-  }
-
-  private ServletContext getServletContext() {
-    return PortalRequestContext.getCurrentInstance().getRequest().getServletContext();
-  }
-
-  public ExoFeatureService getFeatureService() {
-    if (featureService == null) {
-      featureService = ExoContainerContext.getService(ExoFeatureService.class);
-    }
-    return featureService;
   }
 
 }
