1daff08eb66c4317a65998cb6e2ac36b216d824e
Mohamed Amine Krout <mkrout@exoplatform.com>
2025-05-19 10:38:35 +0100

    feat: eXIP10_US02.01: Doc file thumbnail - EXO-78390 (#1115)
    This change will update the Image thumbnail service to make it more
    extensible by adding plugins that will retreive image content from
    different sources.

---
+++ component/file-storage/src/main/java/io/meeds/portal/thumbnail/model/FileContent.java
@@ -0,0 +1,51 @@
+/*
+ * This file is part of the Meeds project (https://meeds.io/).
+ *
+ * Copyright (C) 2025 Meeds Association contact@meeds.io
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+package io.meeds.portal.thumbnail.model;
+
+import java.io.InputStream;
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class FileContent {
+
+  private String      id;
+
+  private String      name;
+
+  private String      mimeType;
+
+  private InputStream content;
+
+  private Date        updatedDate;
+
+  public FileContent(String id, String name, String mimeType, InputStream content) {
+    this.id = id;
+    this.name = name;
+    this.mimeType = mimeType;
+    this.content = content;
+  }
+
+}
