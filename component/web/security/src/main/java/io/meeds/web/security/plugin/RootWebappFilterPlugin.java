a6ca33b45b4e3d4191628854111aa13b33a77d49
Boubaker Khanfir <boubaker.khanfir@gmail.com>
2025-08-19 15:18:01 +0100

    feat: Allow to handle HTTP Request for ROOT.war by extension - exoplatform/exip#21 This change will allow to extend HTTP Requests sent to '/' path (ROOT.war) using an extension mechanism which consists to simply add a Spring Component which extends the 'RootWebappFilterPlugin' interface. This will allow to redirect all WebDav HTTP Requests to the dedicated web archive.

---
+++ component/web/security/src/main/java/io/meeds/web/security/plugin/RootWebappFilterPlugin.java
@@ -0,0 +1,34 @@
+/**
+ * This file is part of the Meeds project (https://meeds.io/).
+ *
+ * Copyright (C) 2020 - 2025 Meeds Association contact@meeds.io
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+package io.meeds.web.security.plugin;
+
+import java.io.IOException;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public interface RootWebappFilterPlugin {
+
+  boolean matches(HttpServletRequest httpRequest, HttpServletResponse httpResponse);
+
+  void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException;
+
+}
