5989f215b9f622470fe27af058d1a0ba42bc8d0e
Boubaker Khanfir <boubaker.khanfir@gmail.com>
2026-01-27 15:03:56 +0100

    feat: Allow to add nested memberships in groups - MEED-10149 - Meeds-io/MIPs#240

--- component/identity/src/main/java/org/exoplatform/services/organization/idm/MembershipImpl.java
+++ component/identity/src/main/java/org/exoplatform/services/organization/idm/MembershipImpl.java
@@ -20,90 +20,41 @@ package org.exoplatform.services.organization.idm;
 
 import org.exoplatform.services.organization.Membership;
 
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
 public class MembershipImpl extends org.exoplatform.services.organization.impl.MembershipImpl implements Comparable {
-    private static final long serialVersionUID = -8572523969777642576L;
 
-    public MembershipImpl() {
+  private static final long serialVersionUID = -8572523969777642576L;
+
+  public MembershipImpl(String membershipType,
+                        String userName,
+                        String groupId,
+                        boolean isInherited) {
+    super(membershipType, userName, groupId, isInherited);
+  }
+
+  public MembershipImpl(String id) {
+    String[] fields = id.split(":");
+
+    // Id can be pure "//" in some cases
+    if (fields[0] != null) {
+      setMembershipType(fields[0]);
     }
-
-    public MembershipImpl(String id) {
-        String[] fields = id.split(":");
-
-        // Id can be pure "//" in some cases
-        if (fields[0] != null) {
-            setMembershipType(fields[0]);
-        }
-        if (fields[1] != null) {
-            setUserName(fields[1]);
-        }
-        if (fields[2] != null) {
-            setGroupId(fields[2]);
-        }
+    if (fields[1] != null) {
+      setUserName(fields[1]);
     }
-
-    public String getId() {
-        StringBuffer id = new StringBuffer();
-
-        if (getMembershipType() != null) {
-            id.append(getMembershipType());
-        }
-        id.append(":");
-        if (getUserName() != null) {
-            id.append(getUserName());
-        }
-        id.append(":");
-        if (getGroupId() != null) {
-            id.append(getGroupId());
-        }
-
-        return id.toString();
+    if (fields[2] != null) {
+      setGroupId(fields[2]);
     }
+  }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (!(o instanceof MembershipImpl)) {
-            return false;
-        }
-
-        MembershipImpl that = (MembershipImpl) o;
-
-        if (getGroupId() != null ? !getGroupId().equals(that.getGroupId()) : that.getGroupId() != null) {
-            return false;
-        }
-        if (getMembershipType() != null ? !getMembershipType().equals(that.getMembershipType()) : that.getMembershipType() != null) {
-            return false;
-        }
-        if (getUserName() != null ? !getUserName().equals(that.getUserName()) : that.getUserName() != null) {
-            return false;
-        }
-
-        return true;
+  public int compareTo(Object o) {
+    if (!(o instanceof Membership) || getUserName() == null) {
+      return 0;
     }
+    Membership m = (Membership) o;
+    return getUserName().compareTo(m.getUserName());
+  }
 
-    @Override
-    public int hashCode() {
-        int result = getMembershipType() != null ? getMembershipType().hashCode() : 0;
-        result = 31 * result + (getUserName() != null ? getUserName().hashCode() : 0);
-        result = 31 * result + (getGroupId() != null ? getGroupId().hashCode() : 0);
-        return result;
-    }
-
-    public int compareTo(Object o) {
-        if (!(o instanceof Membership) || getUserName() == null) {
-            return 0;
-        }
-
-        Membership m = (Membership) o;
-
-        return getUserName().compareTo(m.getUserName());
-    }
-
-    @Override
-    public String toString() {
-        return "MembershipImpl{" + "MembershipType='" + getMembershipType() + '\'' + ", userName='" + getUserName() + '\''
-                + ", GroupId ='" + getGroupId() + '\'' + '}';
-    }
 }
